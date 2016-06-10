47f0fb20ee2553c842522544e78a484603cf26c7
Kiran Mathew <km468@cornell.edu>
2016-06-10 11:22:52 -0700

    remove sorting in bond types

--- polymers/utils.py
+++ polymers/utils.py
@@ -33,13 +33,13 @@ def get_topology(molecule):
     #print len(atoms), len(bonds), len(angles), len(dihedrals)
     atoms = [tuple([str(molecule[x].specie),
                     molecule[x].ff_map]) for x in atoms ]
-    bonds = [x+[tuple(sorted((molecule[x[0]].ff_map,
-                              molecule[x[1]].ff_map)))] for x in bonds ]
+    bonds = [x+[tuple((molecule[x[0]].ff_map,
+                       molecule[x[1]].ff_map))] for x in bonds]
     angles = [x+[tuple(((molecule[x[0]].ff_map,
                          molecule[x[1]].ff_map,
-                         molecule[x[2]].ff_map)))] for x in angles ]
+                         molecule[x[2]].ff_map)))] for x in angles]
     dihedrals =  [x+[tuple((molecule[x[0]].ff_map,
                             molecule[x[1]].ff_map,
                             molecule[x[2]].ff_map,
-                            molecule[x[3]].ff_map))] for x in dihedrals ]
+                            molecule[x[3]].ff_map))] for x in dihedrals]
     return atoms, bonds, angles, dihedrals
